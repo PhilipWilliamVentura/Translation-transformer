acab3a4ef51639c403f9dd01269b50fadfc13a6c
Philip William Ventura <philipwilliamventura@gmail.com>
2025-02-04 20:24:11 -0500

    finished dataset

--- dataset.py
+++ dataset.py
@@ -18,11 +18,62 @@ class BilingualDataset(Dataset):
         self.pad_token = torch.Tensor([tokenizer_src.token_to_id(["[PAD]"])], dtype=torch.int64)
 
     def _len_(self):
-        return len(self.ds)
+        return len(self.ds)   
     
     def __getitem__(self, idx):
         src_target_pair = self.ds[idx]
         src_text = src_target_pair["translation"][self.src_lang]
         tgt_text = src_target_pair["translation"][self.tgt_lang]
 
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) -2
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) -1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence is too long")
+
+        encoder_input = torch.cat(
+            [
+               self.sos_token,
+               torch.tensor(enc_input_tokens, dtype=torch.int64),
+               self.eos_token,
+               torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
+            ],
+        )
+
+        assert encoder_input.size(0)== self.seq_len
+        assert decoder_input.size(0)== self.seq_len
+        assert label.size(0)== self.seq_len 
+
+        return {
+                "encoder_input": encoder_input, 
+                "decoder_input": decoder_input,
+                "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(),
+                "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).int() & causal_mask(decoder_input.size(0)),
+                "label": label,
+                "src_text": src_text,
+                "tgt_text": tgt_text,
+            }
+            
+def causal_mask(size):
+            mask = torch.trio(torch.ones(1, size, size), diagonal=1).type(torch.int)
+            return mask == 0
         
